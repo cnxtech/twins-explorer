525d928a0ce34d0f92169171ef12db14e86061e2
Akshay <akshaycm@hotmail.com>
2019-05-23 14:45:55 +0300

    [Script] Disable automatic SSL

--- script/install.sh
+++ script/install.sh
@@ -55,25 +55,25 @@ server {
             proxy_cache_bypass \$http_upgrade;
     }
 
-    listen [::]:443 ssl ipv6only=on; # managed by Certbot
-    listen 443 ssl; # managed by Certbot
-    ssl_certificate /etc/letsencrypt/live/explorer2.win.win/fullchain.pem; # managed by Certbot
-    ssl_certificate_key /etc/letsencrypt/live/explorer2.win.win/privkey.pem; # managed by Certbot
-    include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
-    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
-}
+#     listen [::]:443 ssl ipv6only=on; # managed by Certbot
+#     listen 443 ssl; # managed by Certbot
+#     ssl_certificate /etc/letsencrypt/live/explorer2.win.win/fullchain.pem; # managed by Certbot
+#     ssl_certificate_key /etc/letsencrypt/live/explorer2.win.win/privkey.pem; # managed by Certbot
+#     include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+#     ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
+# }
 
-server {
-   if ($host = explorer2.win.win) {
-       return 301 https://\$host\$request_uri;
-   } # managed by Certbot
+# server {
+#    if ($host = explorer2.win.win) {
+#        return 301 https://\$host\$request_uri;
+#    } # managed by Certbot
 
-	listen 80 default_server;
-	listen [::]:80 default_server;
+# 	listen 80 default_server;
+# 	listen [::]:80 default_server;
 
-	server_name explorer2.win.win;
-  return 404; # managed by Certbot
-}
+# 	server_name explorer2.win.win;
+#   return 404; # managed by Certbot
+# }
 EOL
     sudo systemctl start nginx
     sudo systemctl enable nginx
